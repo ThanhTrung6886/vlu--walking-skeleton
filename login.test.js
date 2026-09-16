9239ed636c147231835076513abcbcae7b71948b
ThanhTrung6886 <lethanhtrung271106@gmail.com>
2026-09-16 14:30:27 +0700

    Update login.test.js

--- login.test.js
+++ login.test.js
@@ -1,7 +1,7 @@
 const login = require("./login");
 
 test("Đăng nhập đúng tài khoản và mật khẩu", () => {
-    expect(login("admin", "1234")).toBe(true);
+    expect(login("admin", "123")).toBe(true);
 });
 
 test("Đăng nhập sai mật khẩu", () => {
